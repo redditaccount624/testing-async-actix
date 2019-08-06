62b45bc45864e7b37d9e1ff921cd77720dd90766
john <me@example.com>
2019-08-06 05:05:16 -0400

    initial commit

---
+++ src/main.rs
@@ -0,0 +1,42 @@
+#![feature(await_macro, async_await)]
+
+use actix_web::{get, web, App, Error, HttpResponse, HttpServer, Result};
+use actix_web_async_compat::async_compat;
+use futures03::{compat::Future01CompatExt as _, FutureExt as _, TryFutureExt as _};
+use hyper::Client;
+use serde::Deserialize;
+use std::{
+    io,
+    time::{Duration, Instant},
+};
+use tokio::prelude::*;
+
+#[async_compat]
+async fn index() -> Result<HttpResponse> {
+    Ok(HttpResponse::Ok().body("OK"))
+}
+
+#[derive(Debug, Deserialize)]
+struct UserForm {
+    name: String,
+}
+
+#[async_compat]
+async fn index2(form: actix_web::web::Form<UserForm>) -> Result<HttpResponse> {
+    dbg!(form);
+
+    Ok(HttpResponse::Ok().body("OK"))
+}
+
+fn main() {
+    HttpServer::new(|| {
+        App::new()
+            .wrap(actix_web::middleware::Logger::default())
+            .service(index)
+            .service(web::resource("/welcome2").route(web::post().to_async(index2)))
+    })
+    .bind("127.0.0.1:8080")
+    .unwrap()
+    .run()
+    .unwrap();
+}
